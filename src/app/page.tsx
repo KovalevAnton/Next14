096b9339b1753ac045eb351905452c9117790d83
KovalevAnton <a.kovalev@formind.pro>
2024-04-07 20:01:42 -0300

    add components and prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <main>Sas loto</main>;
-}
+import { Button } from "@/components/ui/button";
+
+import FormGenerator from "./form-generator";
+
+const Home = () => {
+  return (
+    <main className="full-w flex justify-center">
+      <FormGenerator />
+    </main>
+  );
+};
+
+export default Home;
